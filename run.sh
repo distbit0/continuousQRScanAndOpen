420e6f2bdfd37abd020a8113e5926440b8ecf7a6
Dist Bit <adp01@pm.me>
2023-06-06 16:16:00 +0200

    run only for 30s

--- run.sh
+++ run.sh
@@ -3,15 +3,5 @@ export DISPLAY=:0
 
 script_path="/home/pimania/Dev/qrScan/run.py"
 
-while true
-do
-    # Check if the script is running
-    if pgrep -f "$script_path" >/dev/null
-    then
-        echo "Script is running"
-    else
-        echo "Script is not running, starting script..."
-        python3 $script_path
-    fi
-    sleep 1
-done
+# Run the script for 30 seconds
+timeout 30 python3 $script_path
